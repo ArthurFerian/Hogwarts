0ffc9d8859ffea6ffc04e497d25465f038ab090c
Arthur Ferian <arthur.ferian@aluno.senai.br>
2025-08-25 10:56:23 -0300

    feat: Criar arquivo dados.js e modifica o server.js

--- server.js
+++ server.js
@@ -1,17 +1,139 @@
 import express from "express";
-import bruxos from "./src/data/bruxos.js";
+import dados from "./src/data/dados.js";
+
+const { bruxos, casas, varinhas, animais, pocoes } = dados;
 
 const app = express();
-const portServer = 3000
+app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send("Vamos de Harry Potter")
+const serverPort = 3000;
+
+app.get("/", (req, res) => {
+    res.send("server funcionando")
 })
 
 app.get("/bruxos", (req, res) => {
-    res.json(bruxos);
+    if (bruxos.length > 0) {
+        res.status(200).json(bruxos);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhum bruxo encontrado!"
+        })
+    }
+})
+
+app.get("/casas", (req, res)=>{
+    if (casas.length > 0) {
+        res.status(200).json(casas);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhuma casa encontrada!"
+        })
+    }
 });
 
-app.listen(portServer, () => {
-    console.log("🧙‍♂️ API dos Bruxos está no ar na porta 3000 !");
-});
+app.get("/varinhas", (req, res)=>{
+    if (varinhas.length > 0) {
+        res.status(200).json(varinhas);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhuma varinha encontrada!"
+        })
+    }
+})
+
+app.get("/animais", (req, res)=>{
+    if (animais.length > 0) {
+        res.status(200).json(animais);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhum animal encontrado!"
+        })
+    }
+})
+
+app.get("/pocoes", (req, res)=>{
+    if (pocoes.length > 0) {
+        res.status(200).json(pocoes);
+    } else {
+        res.status(404).json({
+            mensagem: "Nenhuma poção encontrada!"
+        })
+    }
+})
+
+app.get("/bruxos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const bruxo = bruxos.find((b) => b.id === id);
+  
+    if (bruxo) {
+      res.status(200).json(bruxo);
+    } else {
+      res.status(404).json({
+        mensagem: "bruxo nao encontrado!",
+      });
+    }
+  });
+
+app.get("/casas/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const casa = casas.find((c) => c.id === id);
+  
+    if (casa) {
+      res.status(200).json(casa);
+    } else {
+      res.status(404).json({
+        mensagem: "casa nao encontrada!",
+      });
+    }
+  });
+
+app.get("/varinhas/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const varinha = varinhas.find((v) => v.id === id);
+  
+    if (varinha) {
+      res.status(200).json(varinha);
+    } else {
+      res.status(404).json({
+        mensagem: "varinha nao encontrada!",
+      });
+    }
+  });
+
+app.get("/animais/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const animal = animais.find((a) => a.id === id);
+  
+    if (animal) {
+      res.status(200).json(animal);
+    } else {
+      res.status(404).json({
+        mensagem: "animal nao encontrado!",
+      });
+    }
+  });
+app.get("/pocaos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    const pocao = pocaos.find((p) => p.id === id);
+  
+    if (pocao) {
+      res.status(200).json(pocao);
+    } else {
+      res.status(404).json({
+        mensagem: "pocao nao encontrada!",
+      });
+    }
+  });
+
+
+
+app.listen(serverPort, () => {
+    console.log(`Servidor rodando em http://localhost:${serverPort} `);
+    
+})
---
+++ src/data/dados.js
@@ -0,0 +1,128 @@
+const bruxos = [
+    {
+        id: 1,
+        nome: "Harry Potter",
+        varinha: "Azevinho e pena de fenix",
+        mascote: "Coruja",
+        patrono: "Cervo",
+        especialidade : "sobreviver",
+        vivo: false,
+    },
+    {
+        id: 2,
+        nome: "Hermione Granger",
+        casa: "Grifinória",
+        ano: 7,
+        varinha: "Videira e fibra de coração de dragão, 27cm",
+        mascote: "Gato (Bichento)",
+        patrono: "Lontra",
+        especialidade: "Saber tudo de tudo",
+        vivo: true,
+    },
+]
+
+const casas = [
+    {
+        id: 1,
+        nome: "Grifinória",
+        fundados: "Godric Gryffindor",
+        cores: "Vermelho e Dourado",
+        animal: "Leao"
+    },
+    {
+        id: 2,
+        nome: "Sonserina",
+        fundados: "Salazar Slytherin",
+        cores: "Verde e Prata",
+        animal: "Serpente"
+    },
+    {
+        id: 3,
+        nome: "Corvinal",
+        fundados: "Rowena Ravenclaw",
+        cores: "Azul e Bronze",
+        animal: "Águia"
+    },
+    {
+        id: 4,
+        nome: "Lufa-Lufa",
+        fundados: "Helga Hufflepuff",
+        cores: "Amarelo e Preto",
+        animal: "Texugo"
+    },
+]
+
+const varinhas = [
+    {
+        id: 1,
+        material: "Azevinho",
+        nucleo: "Pena de Fenix",
+        comprimento: "28cm"
+    },
+    {
+        id: 2,
+        material: "Videira",
+        nucleo: "Fibra de Coraçao de Dragao",
+        comprimento: "27cm"
+    },
+    {
+        id: 3,
+        material: "Salgueiro",
+        nucleo: "Pelo de Unicornio",
+        comprimento: "37cm"
+    },
+    {
+        id: 4,
+        material: "Carvalho Ingles",
+        nucleo: "Pelo de Testralip",
+        comprimento: "32cm"
+    },
+]
+
+const animais = [
+    {
+        id: 1,
+        nome: "Coruja",
+        tipo: "Correio/Companhia",
+    },
+    {
+        id: 2,
+        nome: "Gato",
+        tipo: "Companhia",
+    },
+    {
+        id: 3,
+        nome: "Sapo",
+        tipo: "Companhia",
+    },
+    {
+        id: 4,
+        nome: "Rato",
+        tipo: "Companhia",
+    },
+]
+
+const pocoes = [
+    {
+        id: 1,
+        nome: "Polissuco",
+        efeito: "Tranforma na aparencia de outra pessoa"
+    },
+    {
+        id: 2,
+        nome: "Felix Felicis",
+        efeito: "Sorte temporaria"
+    },
+    {
+        id: 3,
+        nome: "Amortentia",
+        efeito: "Poção do amor"
+    },
+    {
+        id: 4,
+        nome: "Veritaserum",
+        efeito: "Força a dizer a verdade"
+    }
+]
+
+export default { bruxos, casas, varinhas, animais, pocoes  };
